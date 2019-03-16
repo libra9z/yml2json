747c0ba233674e1c8a45bdb54be13ef48e58d642
eric <libra9z@163.com>
2019-03-16 09:28:13 +0800

    fix bug

--- yml2json.go
+++ yml2json.go
@@ -8,41 +8,41 @@ import (
 	"os"
 )
 
-func main(){
-	var src,dst,to string
+func main() {
+	var src, dst, to string
 	var gov bool
 	app := &cli.App{
-		Flags: []cli.Flag {
+		Flags: []cli.Flag{
 			&cli.StringFlag{
-				Name: "src",
-				Value: "",
-				Usage: "yml, yaml file or json file to convert. ",
+				Name:        "src",
+				Value:       "",
+				Usage:       "yml, yaml file or json file to convert. ",
 				Destination: &src,
 			},
 			&cli.StringFlag{
-				Name: "dst",
-				Value: "",
-				Usage: "convert to yml, yaml file or json file. ",
+				Name:        "dst",
+				Value:       "",
+				Usage:       "convert to yml, yaml file or json file. ",
 				Destination: &dst,
 			},
 			&cli.StringFlag{
-				Name: "to",
-				Value: "",
-				Usage: "convert dst type,can by yml or json.",
-				Destination:&to,
+				Name:        "to",
+				Value:       "",
+				Usage:       "convert dst type,can by yml or json.",
+				Destination: &to,
 			},
 			&cli.BoolFlag{
-				Name: "var",
-				Value: true,
-				Usage: "convert dst to golang var.",
+				Name:        "var",
+				Value:       true,
+				Usage:       "convert dst to golang var.",
 				Destination: &gov,
 			},
 		},
 		Action: func(c *cli.Context) error {
 			if to == "json" {
-				toJson(src,dst,gov)
-			}else if to == "yml" || to == "yaml" {
-				toYaml(src,dst)
+				toJson(src, dst, gov)
+			} else if to == "yml" || to == "yaml" {
+				toYaml(src, dst)
 			}
 			return nil
 		},
@@ -51,8 +51,7 @@ func main(){
 	app.Run(os.Args)
 }
 
-
-func toJson(src,dst string,gov bool) {
+func toJson(src, dst string, gov bool) {
 	buffer, err := ioutil.ReadFile(src)
 	j2, err := yaml.YAMLToJSON(buffer)
 	if err != nil {
@@ -60,27 +59,27 @@ func toJson(src,dst string,gov bool) {
 		return
 	}
 
+	if gov {
+		createVarFile(string(j2))
+	}
+
 	var mf *os.File
 	if checkFileIsExist(dst) { //如果文件存在
-		mf, err = os.OpenFile(dst,os.O_TRUNC|os.O_CREATE,os.ModePerm) //打开文件
+		mf, err = os.OpenFile(dst, os.O_TRUNC|os.O_CREATE, os.ModePerm) //打开文件
 	} else {
 		mf, err = os.Create(dst) //创建文件
 	}
 	if err != nil {
-		fmt.Printf("不能创建或者打开文件：%v\n",err)
+		fmt.Printf("不能创建或者打开文件：%v\n", err)
 		return
 	}
-	defer  mf.Close()
+	defer mf.Close()
 
 	mf.WriteString(string(j2))
 
-	if gov {
-		createVarFile(string(j2))
-	}
 }
 
-
-func toYaml(src,dst string) {
+func toYaml(src, dst string) {
 	buffer, err := ioutil.ReadFile(src)
 
 	j2, err := yaml.JSONToYAML(buffer)
@@ -91,15 +90,15 @@ func toYaml(src,dst string) {
 
 	var mf *os.File
 	if checkFileIsExist(dst) { //如果文件存在
-		mf, err = os.OpenFile(dst,os.O_TRUNC|os.O_CREATE,os.ModePerm) //打开文件
+		mf, err = os.OpenFile(dst, os.O_TRUNC|os.O_CREATE, os.ModePerm) //打开文件
 	} else {
 		mf, err = os.Create(dst) //创建文件
 	}
 	if err != nil {
-		fmt.Printf("不能创建或者打开文件：%v\n",err)
+		fmt.Printf("不能创建或者打开文件：%v\n", err)
 		return
 	}
-	defer  mf.Close()
+	defer mf.Close()
 
 	mf.WriteString(string(j2))
 
@@ -116,23 +115,23 @@ func checkFileIsExist(filename string) bool {
 	return exist
 }
 
-func createVarFile( s string) {
+func createVarFile(s string) {
 	mfile := "docs/docsvar.go"
 	var mf *os.File
 	var err error
 	if checkFileIsExist(mfile) { //如果文件存在
-		mf, err = os.OpenFile(mfile,os.O_TRUNC|os.O_CREATE,os.ModePerm) //打开文件
+		mf, err = os.OpenFile(mfile, os.O_TRUNC|os.O_CREATE, os.ModePerm) //打开文件
 	} else {
 		mf, err = os.Create(mfile) //创建文件
 	}
 	if err != nil {
-		fmt.Printf("不能创建或者打开文件：%v\n",err)
+		fmt.Printf("不能创建或者打开文件：%v\n", err)
 		return
 	}
-	defer  mf.Close()
+	defer mf.Close()
 
 	var c = " package docs"
-	var c1=	"	var doc = ` "
+	var c1 = "	var doc = ` "
 
 	mf.WriteString(c)
 	mf.WriteString("\n")
